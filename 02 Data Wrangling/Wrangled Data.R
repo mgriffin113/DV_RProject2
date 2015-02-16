3a6ddafcc18e32aaf4e6cd8f5b68e72a6601eaea
Michael Griffin <mgriffin113@yahoo.com>
2015-02-16 17:51:41 -0600

    Created 3 workflows for wrangling data and updated other files, still missing graphs

--- 02 Data Wrangling/Wrangled Data.R
+++ 02 Data Wrangling/Wrangled Data.R
@@ -1,1 +1,30 @@
-#Wrangle the data
+# Wrangle the data
+
+# This dataset shows the number of trips each particular vehicle made
+# A trip is defined as the ride from one stop to the next
+numOfTripsPerVehicle<- 
+  df1 %>%
+  group_by(VEHICLE_ID) %>%
+  summarise(n())
+
+# This dataset shows the average distance between stops that each vehicle has along each route it drives
+avgDistanceToNextStop<-
+  df1 %>%
+  group_by(INFERRED_ROUTE_ID, VEHICLE_ID) %>%
+  summarise(mean(NEXT_SCHEDULED_STOP_DISTANCE))
+
+df<-
+  df1 %>%
+  separate(TIME_RECEIVED,c("DATE_RECEIVED","HOUR_RECEIVED"),sep=" ") %>%
+  filter(DATE_RECEIVED=="2014-08-02") %>%
+  group_by(VEHICLE_ID) %>%
+  arrange(HOUR_RECEIVED) %>%
+  separate(HOUR_RECEIVED,c("HOUR","MINUTE","SECONDS"),sep=":") %>%
+  mutate(HOUR_DIFF = as.integer(lead(HOUR))-as.integer(HOUR), MIN_DIFF = as.integer(lead(MINUTE))-as.integer(MINUTE), SEC_DIFF = as.integer(lead(SECONDS))-as.integer(SECONDS)) %>%
+  mutate(TIME_DIFF = HOUR_DIFF + 60 * MIN_DIFF + SEC_DIFF) %>%
+  filter(TIME_DIFF > 0) %>%
+  group_by(VEHICLE_ID) %>%
+  summarise(AVG_TIME_DIFF = mean(TIME_DIFF),AVG_STOP_DIST = mean(NEXT_SCHEDULED_STOP_DISTANCE)) %>%
+  mutate(AVG_FPS = AVG_STOP_DIST/AVG_TIME_DIFF)
+  
+
--- 03 Visualizations/Display Data Frames.R
+++ 03 Visualizations/Display Data Frames.R
@@ -1,3 +1,5 @@
+library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+
 # Head of transportation dataset:
 
-print(head(df1))
+print(tbl_df(df1))
