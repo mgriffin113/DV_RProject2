c0d393a37066c06b87a99a11f2f5ba588eade491
Jacob Rachiele <jrachiele@utexas.edu>
2015-02-16 21:12:41 -0600

    R saved distance variables as factors because of the commas, so replaced those and changed to numeric.

--- 01 Data/Create Data Frames.r
+++ 01 Data/Create Data Frames.r
@@ -4,3 +4,6 @@ library("RCurl")
 # We use the RCurl function getURL to pull in the BusData table stored on Michael's oracle account.
 
 bus_sample_data <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from BusData"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_mjg2833', PASS='orcl_mjg2833', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+
+bus_sample_data$DISTANCE_ALONG_TRIP <- sub(",", "", bus_sample_data$DISTANCE_ALONG_TRIP) 
+bus_sample_data$DISTANCE_ALONG_TRIP <- as.numeric(bus_sample_data$DISTANCE_ALONG_TRIP)
--- 03 Visualizations/Display Data Frames.R
+++ 03 Visualizations/Display Data Frames.R
@@ -2,4 +2,4 @@ library("dplyr")
 
 # Display subset of data using dplyr tbl_df function to transform data frame for viewing.
 
-print(tbl_df(bus_sample_data ))
+bus_sample_data %>% tbl_df %>% print()
